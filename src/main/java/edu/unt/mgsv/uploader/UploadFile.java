7bd5be7f591dc958912ee1d1df5018f4b4fb9f39
Anil <anil@aniljava.com>
2012-06-15 12:22:50 -0500

    Implemented Configuration and URL upload

--- src/main/java/edu/unt/mgsv/uploader/UploadFile.java
+++ src/main/java/edu/unt/mgsv/uploader/UploadFile.java
@@ -3,39 +3,64 @@ package edu.unt.mgsv.uploader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Properties;
 import java.util.zip.GZIPInputStream;
+
+import javax.xml.ws.BindingProvider;
 
 import edu.unt.mgsv.client.MGSVService;
 import edu.unt.mgsv.client.MGSVServiceService;
 
 public class UploadFile {
 	public static void main(String[] args) throws Exception {
-		
-		
-		
-		if (args.length < 1){
+
+		if (args.length < 1) {
 			System.out.println("Running a demo mode with a.gz and s.gz in current dir if they exist");
-			args = new String[]{"s.gz", "a.gz"};			
-			
-		}
-			
+			args = new String[] { "s.gz", "a.gz" };
 
-		String synteny = fileToString(new File(args[0]));
-		String annotation = null;
-		String email = null;
-
-		if (args.length > 1) {
-			annotation = fileToString(new File(args[1]));;
-		}
-
-		if (args.length > 2) {
-			email = args[2];
 		}
 
 		MGSVServiceService service = new MGSVServiceService();
 		MGSVService impl = service.getMGSVServicePort();
 
-		System.out.println(impl.uploadData(synteny, annotation, email));
+		// Alternate remote url if it exists
+		if (new File("config.properties").exists()) {
+			Properties config = new Properties();
+			config.load(new FileInputStream("confi.properties"));
+			String remote = config.getProperty("remote");
+			if (remote != null) {
+				((BindingProvider) impl).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, remote);
+			}
+		}
+
+		String result = "-1";
+
+		if (args[0].startsWith("http")) {
+			// URL
+			String synteny = args[0];
+			String annotation = (args.length > 1) ? args[1] : null;
+			String email = (args.length > 2) ? args[2] : null;
+
+			result = impl.uploadURL(synteny, annotation, email);
+
+		} else {
+			// File
+			String synteny = fileToString(new File(args[0]));
+			String annotation = null;
+			String email = null;
+
+			if (args.length > 1) {
+				annotation = fileToString(new File(args[1]));;
+			}
+
+			if (args.length > 2) {
+				email = args[2];
+			}
+
+			result = (impl.uploadData(synteny, annotation, email));
+		}
+
+		System.out.println(result);
 
 	}
 
